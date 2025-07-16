5403ca303098e83e866fab0f3e59736e9fa09b6b
Alexandre Morgaut <alexandre.morgaut@capgemini.com>
2025-07-17 00:04:55 +0200

    build: bootstrap sonar-plugin app

---
+++ apps/sonar-plugin/vite.config.js
@@ -0,0 +1,25 @@
+import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/view.js`,
+        assetFileNames: `assets/view.css`
+      }
+    }
+  }
+})
