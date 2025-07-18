82b816926e8f2625ad39a32470ba7f7a106f33fc
Alexandre Morgaut <alexandre.morgaut@capgemini.com>
2025-07-18 14:21:02 +0200

    build: update github action and playwright support

--- shared/sonar-services/vite.config.js
+++ shared/sonar-services/vite.config.js
@@ -1,15 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    target: 'esnext'
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    target: 'esnext',
+    lib: {
+      entry: resolve(__dirname + '/src/index.js'),
+      name: 'sonar-service',
+      fileName: 'sonar-service'
     }
-  }
+  },
 })
