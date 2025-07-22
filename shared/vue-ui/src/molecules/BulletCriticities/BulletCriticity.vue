501fa1ac7e02763a8a215f1547d594fd4c336111
Alexandre Morgaut <alexandre.morgaut@capgemini.com>
2025-07-22 21:21:35 +0200

    feat(web-ui): Add BulletCriticities Molecule

---
+++ shared/vue-ui/src/molecules/BulletCriticities/BulletCriticity.vue
@@ -0,0 +1,64 @@
+
+<script setup>
+import { computed } from 'vue';
+import IconArrowRight from '../../atoms/Icons/IconArrowRight.vue';
+import ImpactTag from '../../atoms/ImpactTag/ImpactTag.vue';
+
+const props = defineProps({
+  count: { type: Number, required: true },
+  type: { type: String, required: true,
+    validator(value) {
+      return ['rule', 'issue'].includes(value)
+    }
+  },
+  impact: { type: String, required: false, default: 'Optimized',
+    validator(value) {
+      return ['Low', 'Medium', 'High', 'Optimized'].includes(value)
+    }
+  },
+  metricTag: { type: String, required: true,
+    validator(value) {
+      return ['CPU', 'RAM', 'Disk', 'Network'].includes(value)
+    }
+  },
+  projectLink: { type: String, required: true },
+});
+
+const isRule = computed(() => props.type === 'rule');
+const hasIssues = computed(() => !isRule.value && props.count > 0);
+const customLabel = computed(() => isRule.value ? `${props.metricTag} rules` : 'impact issues');
+
+function onClickOpenIssues(impact) {
+  window.open(`${props.projectLink}&severities=${impact.toUpperCase()}`);
+}
+</script>
+
+<template>
+  <li
+    :class="[
+      'criticity',
+      'bullet',
+      `bullet-${impact}-impact`,
+      { clickable: isIssues },
+    ]"
+    @click="hasIssues ? onClickOpenIssues(impact) : null"
+    @keyUp="hasIssues ? onClickOpenIssues(impact) : null"
+  >
+    &nbsp;&nbsp;<ImpactTag :impact="`${impact}`" 
+                           :customLabel="`${customLabel}`" />
+    <strong :class="{ 'no-issues': !hasIssues }">{{ count }}</strong>
+    <IconArrowRight v-if="hasIssues" />
+  </li>
+</template>
+
+<style scoped>
+.bullet-minor-impact::before {
+  background-color: #fecb02;
+}
+.bullet-major-impact::before {
+  background-color: #ff8e12;
+}
+.bullet-critical-impact::before {
+  background-color: #e30021;
+}
+</style>
