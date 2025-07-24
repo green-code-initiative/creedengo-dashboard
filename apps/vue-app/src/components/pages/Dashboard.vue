f0da5721ffd34eb039ec554342fd612e6ac31601
Alexandre Morgaut <alexandre.morgaut@capgemini.com>
2025-07-24 21:43:36 +0200

    feat(vue-app): Add settings page

---
+++ apps/vue-app/src/components/pages/Dashboard.vue
@@ -0,0 +1,15 @@
+<script setup>
+import ScoreWidget from '../Widgets/Score/ScoreWidget.vue';
+</script>
+
+<template>
+  <header>
+    <h1>Creedengo Dashboard</h1>
+  </header>
+  <main>
+    <ScoreWidget
+      project-key="my-project-key"
+      branch="main"
+    />
+  </main>
+</template>
