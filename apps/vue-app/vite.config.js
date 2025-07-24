f1d0c51d5c2852b02c24aebdbdcc126183cffb90
Alexandre Morgaut <alexandre.morgaut@capgemini.com>
2025-07-24 21:46:19 +0200

    build(vue-app): support older browsers

--- apps/vue-app/vite.config.js
+++ apps/vue-app/vite.config.js
@@ -4,11 +4,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
+/**
+ * 
+ * @see https://vitejs.dev/config/
+ * @see https://ecoresponsable.numerique.gouv.fr/publications/referentiel-general-ecoconception/#specifications
+ * @see https://w3c.github.io/sustainableweb-wsg/#visitor-constraints
+ */
 export default defineConfig({
   plugins: [vue(), VueDevTools()],
   build: {
-    target: 'esnext',
+    target: 'es2015', // Compliance with RGESN 2.4, WSG 2.2
     rollupOptions: {
       external: [],
       output: {
