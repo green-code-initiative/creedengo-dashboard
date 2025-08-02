577cf0be749e0e0b71e6f7e352ca490a7949818e
Alexandre Morgaut <alexandre.morgaut@capgemini.com>
2025-08-02 20:27:42 +0200

    build(vue-ui): move icons to design-tokens

--- shared/vue-ui/src/index.js
+++ shared/vue-ui/src/index.js
@@ -1,11 +1,12 @@
+// Design Tokens
+import IconArrowRight from './design-tokens/icons/IconArrowRight.vue'
+import IconBug from './design-tokens/icons/IconBug.vue'
+import IconCpu from './design-tokens/icons/IconCpu.vue'
+import IconDisk from './design-tokens/icons/IconDisk.vue'
+import IconMaintenance from './design-tokens/icons/IconMaintenance.vue'
+import IconNetwork from './design-tokens/icons/IconNetwork.vue'
+import IconRam from './design-tokens/icons/IconRam.vue'
 // Atoms
-import IconArrowRight from './atoms/Icons/IconArrowRight.vue'
-import IconBug from './atoms/Icons/IconBug.vue'
-import IconCpu from './atoms/Icons/IconCpu.vue'
-import IconDisk from './atoms/Icons/IconDisk.vue'
-import IconMaintenance from './atoms/Icons/IconMaintenance.vue'
-import IconNetwork from './atoms/Icons/IconNetwork.vue'
-import IconRam from './atoms/Icons/IconRam.vue'
 import ImpactTag from './atoms/ImpactTag/ImpactTag.vue'
 // Molecules
 import AbcdeScore from './molecules/AbcdeScore/AbcdeScore.vue'
@@ -19,9 +20,9 @@ import SettingsForm from './molecules/SettingsForm/SettingsForm.vue'
 import ScoreBlock from './organisms/ScoreBlock/ScoreBlock.vue'
 import TagCard from './organisms/TagCard/TagCard.vue'
 
-
-// Atoms
+// Design Tokens
 export { IconArrowRight, IconBug, IconCpu, IconDisk, IconMaintenance, IconNetwork, IconRam }
+// Atoms
 export { ImpactTag }
 // Molecules
 export { AbcdeScore, BulletCriticities, PieChart, RuleIconTag, RuleCriticities, SettingsForm, TooltipBox }
