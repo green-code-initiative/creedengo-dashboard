7ee8be737cd1a66eed25f12a1d5be7e36e4f67fe
Alexandre Morgaut <alexandre.morgaut@capgemini.com>
2025-09-07 11:28:36 +0200

    feat(sonar-services): add rule list and detail

--- shared/sonar-services/src/index.js
+++ shared/sonar-services/src/index.js
@@ -7,14 +7,18 @@ import { handlers } from './api/mock/msw-handler.js'
 import * as issues from './api/issues/sonar.issues.search.api.js'
 import * as measures from './api/measures/sonar.measures.component.api.js'
 import * as branches from './api/project-branches/sonar.project-branches.list.api.js'
+import * as pullRequests from './api/project-pull-requests/sonar.project-pull-requests.list.api.js'
+import rules from './api/rules/index.js'
 
 export default {
     initSettings: request.initSettings,
     ...request,
     ...i18n,
     ...toast,
+    ...branches,
     ...issues,
     ...measures,
-    ...branches,
+    ...pullRequests,
+    ...rules,
     handlers
 }
