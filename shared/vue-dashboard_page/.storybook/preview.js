01d6f8275ae809a4ea9972ec64c505c6e09793fb
Ombrustou <77807271+Ombrustou@users.noreply.github.com>
2026-05-19 17:13:51 +0200

    storybook

---
+++ shared/vue-dashboard_page/.storybook/preview.js
@@ -0,0 +1,15 @@
+export default {
+  parameters: {
+    options: {
+      storySort: {
+        order: ['Intro', 'Design Tokens', 'Atoms', 'Molecules', 'Organisms', 'Page Templates'],
+      },
+    },
+    docs: {
+      canvas: {
+        sourceState: 'shown',
+      },
+      codePanel: true,
+    },
+  },
+};
